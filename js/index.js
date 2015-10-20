ca4544765d8d2952ae80925dfc075a82b2408825
Ashley Wilson <awilson@angelseyeinc.com>
2015-10-20 15:21:22 -0500

    completion of tutorial step 1

--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
-var data = [4, 8, 15, 16, 23, 42];
+var data = [3, 6, 9, 10, 12, 39];
 
 var x = d3.scale.linear()
     .domain([0, d3.max(data)])
     .range([0, 420]);
 
 d3.select(".chart")
-  .selectAll("div")
-    .data(data)
-  .enter().append("div")
-    .style("width", function(d) { return x(d) + "px"; })
-    .text(function(d) { return d; });
+	.selectAll('div')
+		.data(data)
+	.enter().append("div")
+		.style("width", function(d){ return x(d) + "px"; })
+		.text(function(d){ return d; });
+
+		
